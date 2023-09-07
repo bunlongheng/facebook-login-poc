a52393aeefede9acaf74a51b92f6ddd944d2c87c
Bunlong Heng <bheng.code@gmail.com>
2023-09-07 16:50:27 -0400

    add in config_id;

--- src/LoginWithFacebook.js
+++ src/LoginWithFacebook.js
@@ -3,13 +3,18 @@ import React from "react";
 
 function LoginWithFacebook() {
     const handleLogin = () => {
-        window.FB.login(function (response) {
-            if (response.authResponse) {
-                console.log("Successfully logged in with Facebook", response);
-            } else {
-                console.log("Login failed");
+        window.FB.login(
+            function (response) {
+                if (response.authResponse) {
+                    console.log("Successfully logged in with Facebook", response);
+                } else {
+                    console.log("Login failed");
+                }
+            },
+            {
+                config_id: "3396959963918383",
             }
-        });
+        );
     };
 
     return <button onClick={handleLogin}>Login with Facebook</button>;
